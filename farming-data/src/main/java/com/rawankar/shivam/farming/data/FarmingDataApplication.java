c77b394b3dbd5b895c22cc5bcc6be65c3b580aca
RawankarS <126663500+RawankarS@users.noreply.github.com>
2023-12-20 19:48:37 +0530

    first commit of project

---
+++ farming-data/src/main/java/com/rawankar/shivam/farming/data/FarmingDataApplication.java
@@ -0,0 +1,13 @@
+package com.rawankar.shivam.farming.data;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FarmingDataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FarmingDataApplication.class, args);
+	}
+
+}
---
+++ farming-data/src/test/java/com/rawankar/shivam/farming/data/FarmingDataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rawankar.shivam.farming.data;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FarmingDataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
